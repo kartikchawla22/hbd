03c60c554dcd983df00c808fe5f82892f7792772
Kartik Chawla <kartik.chawla@kelltontech.com>
2018-04-19 20:08:52 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit,  ElementRef } from '@angular/core';
+import { scrollTo  } from 'ng2-utils';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { ConstantsService } from './constants.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnInit {
+  constructor(public elementRef :ElementRef, private constant: ConstantsService ){}
   title = 'app works!';
+  id = 's1'; hid = 'h1'; wid='w1';
+  myStyle : Object = {};
+  myParams : Object = {};
+  scrollTo(selector, parentSelector, horizontal) {
+    scrollTo(
+      selector,       // scroll to this
+      parentSelector, // scroll within (null if window scrolling)
+      horizontal,     // is it horizontal scrolling
+      0              // distance from top or left
+    );
+  }
+  ngOnInit(){
+    this.myStyle = {
+      position: 'absolute',
+      width: '100%',
+      height: '100%'
+    }
+    this.myParams = this.constant.particles;
+  }
+  ngAfterViewInit(){
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'rgba(37, 194, 210, 0.81)';
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { Ng2ScrollableModule } from 'ng2-scrollable';
+import { ParticlesModule } from 'angular-particle';
+import {ConstantsService  } from '../app/constants.service'
 
 @NgModule({
   declarations: [
@@ -12,9 +14,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2ScrollableModule,
+    ParticlesModule
   ],
-  providers: [],
+  providers: [
+    ConstantsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
